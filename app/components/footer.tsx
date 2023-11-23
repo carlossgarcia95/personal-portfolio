a2a63483dfdb7e8221050d5435c06babb7ed7de6
Carlos Garcia <carlosgarcia3995@gmail.com>
2023-11-23 13:25:01 -0800

    Add ThemeSwitch to mobile footer.

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,10 +1,14 @@
 import Link from "next/link";
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import ThemeSwitch from "./theme-switch";
 
 const Footer = () => {
   return (
     <section className="bg-slate-200 dark:bg-slate-950">
-      <footer className="p-5 text-center space-y-4 border-t border-slate-300 dark:border-slate-600 mx-auto max-w-7xl w-11/12">
+      <footer className="p-5 space-y-4 md:pt-0 text-center border-t border-slate-300 dark:border-slate-600 mx-auto max-w-7xl w-11/12">
+        <div className="flex self-center justify-center md:hidden">
+          <ThemeSwitch />
+        </div>
         <div className="flex justify-center items-center gap-2">
           <Link href="https://linkedin.com/in/carlossgarcia95" target="_blank">
             <AiFillLinkedin size={34} className="hover:cursor-pointer" />
@@ -17,8 +21,8 @@ const Footer = () => {
           &copy; 2023 Carlos Garcia. All rights reserved.
         </p>
         <p className="text-xs">
-          <span className="font-semibold">About website:</span> Designed
-          and developed by Carlos Garcia.
+          <span className="font-semibold">About website:</span> Designed and
+          developed by Carlos Garcia.
         </p>
       </footer>
     </section>
