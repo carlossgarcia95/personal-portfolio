c77baaad334d4a7ee43f982997440cb0eedc224e
Carlos Garcia <carlosgarcia3995@gmail.com>
2023-12-05 16:00:54 -0800

    Add GoogleAnalyticsScript to layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,18 @@
 import type { Metadata } from "next";
+import { Toaster } from "sonner";
+import GoogleAnalyticsScript from "./GoogleAnalyticsScript";
 import Footer from "./components/footer";
 import Navbar from "./components/navbar";
+import ScrollToTopButton from "./components/scroll-to-top-button";
 import ThemeSwitch from "./components/theme-switch";
 import { inter } from "./components/ui/fonts";
 import ThemeContextProvider from "./context/theme-context";
-import { Toaster } from "sonner";
 import "./globals.css";
-import ScrollToTopButton from "./components/scroll-to-top-button";
 
 export const metadata: Metadata = {
   title: "Carlos Garcia Portfolio",
-  description: "Carlos' Garcia professional portfolio. Includes project management and full-stack development.",
+  description:
+    "Carlos' Garcia professional portfolio. Includes project management and full-stack development.",
 };
 
 export default function RootLayout({
@@ -21,8 +23,9 @@ export default function RootLayout({
   return (
     <html lang="en" className="!scroll-smooth">
       <head>
-      <link rel="icon" href="/logo.png" sizes="any" />
+        <link rel="icon" href="/logo.png" sizes="any" />
       </head>
+      <GoogleAnalyticsScript />
       <body
         className={`${inter.className} antialiased bg-slate-50 text-slate-900 font-medium dark:bg-slate-900 dark:text-slate-50 dark:text-opacity-90`}
       >
@@ -31,7 +34,7 @@ export default function RootLayout({
           <main className="mx-auto">{children}</main>
           <ScrollToTopButton />
           <ThemeSwitch className="hidden md:flex fixed bottom-5 left-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950" />
-          <Toaster richColors/>
+          <Toaster richColors />
           <Footer />
         </ThemeContextProvider>
       </body>
